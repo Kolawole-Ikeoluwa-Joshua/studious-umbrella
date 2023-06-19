2a690a5242d556559011abd35e29e48dea52bebd
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-06-19 07:26:02 +0100

    emitting events for posts

--- posts/index.js
+++ posts/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,7 +20,7 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     // generate post id
     const id = randomBytes(4).toString('hex');
 
@@ -27,6 +29,14 @@ app.post('/posts', (req, res) => {
     posts[id] = {
         id, title
     };
+
+    // emit an event
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
 
     res.status(201).send(posts[id]);
 
