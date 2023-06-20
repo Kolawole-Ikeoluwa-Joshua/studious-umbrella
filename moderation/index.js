395a9b294bd5a8b3133ea3dee227452375b25cee
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-06-20 08:23:57 +0100

    creation of moderation service

---
+++ moderation/index.js
@@ -0,0 +1,17 @@
+// comment moderation service
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+
+});
+
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
