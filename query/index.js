eed777073b0f0686617974c5783d8657b594acd7
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-06-21 16:25:23 +0100

    dealing with missing events with event sync

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 
 const app = express();
@@ -11,15 +12,8 @@ app.use(cors());
 // store all posts and comments
 const posts = {};
 
-
-app.get('/posts', (req,res) => {
-    res.send(posts);
-});
-
-
-app.post('/events', (req,res) => {
-    const { type, data } = req.body;
-
+// event handling logic as reusable function
+const handleEvent = (type, data) => {
     if (type == 'PostCreated') {
         const {id, title } = data;
 
@@ -46,13 +40,33 @@ app.post('/events', (req,res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);    
+app.get('/posts', (req,res) => {
+    res.send(posts);
+});
+
+
+app.post('/events', (req,res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);    
     
     res.send({});
 
 });
 
-app.listen(4002, () => {
-    console.log('Listening on 4002');
-});
+app.listen(4002, async () => {
+    console.log("Listening on 4002");
+    try {
+      const res = await axios.get("http://localhost:4005/events");
+   
+      for (let event of res.data) {
+        console.log("Processing event:", event.type);
+   
+        handleEvent(event.type, event.data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
