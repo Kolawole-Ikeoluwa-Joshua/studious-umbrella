2a58a1f27a5df95172a46bd6ebc66a9bccfa52c1
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-06-26 04:35:12 +0100

    updating event bus urls

--- comments/index.js
+++ comments/index.js
@@ -31,7 +31,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     commentsByPostId[req.params.id] = comments;
 
     // emit event
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
@@ -64,7 +64,7 @@ app.post('/events', async (req,res) => {
         // update comment status
         comment.status = status;
 
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentUpdated',
             data: {
                 id,
