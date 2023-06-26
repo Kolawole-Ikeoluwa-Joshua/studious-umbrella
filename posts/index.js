c14c8b106b029546466c0c64eaf98c97950f8f88
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-06-26 04:04:40 +0100

    communication between post and event-bus

--- posts/index.js
+++ posts/index.js
@@ -31,7 +31,7 @@ app.post('/posts', async (req, res) => {
     };
 
     // emit an event
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
